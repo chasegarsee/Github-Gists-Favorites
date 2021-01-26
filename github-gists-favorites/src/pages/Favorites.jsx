4cb73082a1e92d6d25b405a706012d4bd9c8a7ff
Chase Garsee <chasegarsee@gmail.com>
2021-01-26 09:00:48 -0600

    Adds page structure and sets up routes

---
+++ github-gists-favorites/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./styles.css"
+
+export default function Favorites() {
+    return (
+        <div>
+            <h1>Favorites</h1>
+        </div>
+    )
+}
---
+++ github-gists-favorites/src/pages/index.js
@@ -0,0 +1,5 @@
+import Dashboard from "./Dashboard";
+import Favorites from "./Favorites";
+import Error from "./Error";
+
+export { Dashboard, Favorites, Error };
