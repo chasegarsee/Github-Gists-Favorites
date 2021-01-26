e03c4a76fca93c31c498e4771b544dc7585e567e
Chase Garsee <chasegarsee@gmail.com>
2021-01-26 11:50:51 -0600

    Added this ability to remove favorites from /favorites view and local database.

--- github-gists-favorites/src/pages/Favorites.jsx
+++ github-gists-favorites/src/pages/Favorites.jsx
@@ -1,12 +1,22 @@
 import React, { useContext } from 'react'
 import { GitContext } from "../context/context"
-import { Container, Card, Row, Col } from "react-bootstrap"
+import { Container, Card, Row, Col, Button } from "react-bootstrap"
 import { Star } from "react-feather"
 import GistCode from "../components/GistCode"
+import { useIndexedDB } from "react-indexed-db"
 import "./styles.css"
 
 export default function Favorites() {
-    const { favoritedGists } = useContext(GitContext)
+    const { favoritedGists, updateStateFromLocalDatabase} = useContext(GitContext)
+    const { deleteRecord } = useIndexedDB("gists")
+
+    const handleDelete = async (e) => {
+      await deleteRecord(e).then(() => {
+        console.log("deleted id: ", e)
+        updateStateFromLocalDatabase()
+      })
+    }
+  
     
     return (
     <Container className="mt-3">
@@ -18,11 +28,17 @@ export default function Favorites() {
                 <Row className="justify-content-between">
                   <Card.Title>{gist.filename}</Card.Title>
                   <Card.Text>
-                    <Star
-                      color="gold"
-                      fill="gold"
-                      size={25}
-                    />
+                  <Button
+                    id="favorite"
+                    variant="outline-warning"
+                    onClick={() => { handleDelete(gist.id) }}
+                    >
+                    <Star 
+                        color="gold"
+                        fill="gold" 
+                        className="clear-button"
+                        />
+                  </Button>
                   </Card.Text>
                 </Row>
               </Card.Header>
