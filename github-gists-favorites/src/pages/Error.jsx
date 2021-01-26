30838d3ca1f2c8580b152843c795865016f96d48
Chase Garsee <chasegarsee@gmail.com>
2021-01-26 09:05:47 -0600

    Sets up a simple error page to handle if/when the user decides to manually navigate to an unknown route.

--- github-gists-favorites/src/pages/Error.jsx
+++ github-gists-favorites/src/pages/Error.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { Container, Jumbotron, Button, Row, Col } from "react-bootstrap"
+import { Link } from "react-router-dom"
 export default function Error() {
     return (
-        <div>
-            <h1>Error</h1>
-        </div>
+        <Container className="vh-100 justify-content-md-center">
+            <Row align="center" className="h-100 justify-content-md-center align-items-center">
+                <Col>
+                    <Jumbotron>
+                        <h1>404</h1>
+                        <p>Sorry! The page you are looking for cannot be found.</p>
+                        <Link to="/">
+                            <Button variant="info">Go back</Button>
+                        </Link>
+                    </Jumbotron>    
+                </Col> 
+            </Row>
+        </Container>
     )
 }
