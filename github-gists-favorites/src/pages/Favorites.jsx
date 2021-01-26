c8f54987e88b42056f39da2189811595a45c533d
Chase Garsee <chasegarsee@gmail.com>
2021-01-26 11:09:16 -0600

    Displays favorited gists in the /favorites route. Favorites persists through refresh and closing / reopening the browser window.

--- github-gists-favorites/src/pages/Favorites.jsx
+++ github-gists-favorites/src/pages/Favorites.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GitContext } from "../context/context"
+import { Container, Card, Row, Col } from "react-bootstrap"
+import { Star } from "react-feather"
+import GistCode from "../components/GistCode"
 import "./styles.css"
 
 export default function Favorites() {
+    const { favoritedGists } = useContext(GitContext)
+    
     return (
-        <div>
-            <h1>Favorites</h1>
-        </div>
+    <Container className="mt-3">
+      <Row md="auto" sm="1" xs="1">
+        {favoritedGists.map((gist, key) => (
+          <Col key={key}>
+            <Card className="mb-4" key={key}>
+              <Card.Header>
+                <Row className="justify-content-between">
+                  <Card.Title>{gist.filename}</Card.Title>
+                  <Card.Text>
+                    <Star
+                      color="gold"
+                      fill="gold"
+                      size={25}
+                    />
+                  </Card.Text>
+                </Row>
+              </Card.Header>
+              <Card.Body>
+                <GistCode gistId={gist.id} />
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
     )
 }
